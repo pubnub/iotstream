3a045c52427a7d216289143360375c729f3ec6ea
Cody Byrnes <codyb@pubnub.com>
2015-01-26 14:59:00 -0800

    New nav and intro box

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 $('a').click(function(){
     $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 50
     }, 500);
     return false;
 });
+
+var introVideo = document.getElementById('introVideo');
+//introVideo.onended = function () {
+//    console.log("Ended");
+//    introVideo.currentTime = 0;
+//    introVideo.play();
+//};
+
+//$("#introVideo").on('ended', function(){
+//    console.log("Ended");
+//    console.log(introVideo.currentTime);
+//    introVideo.currentTime = 607;
+//    introVideo.play();
+//})
+
+//introVideo.addEventListener('ended', function () {
+//    this.currentTime = 0;
+//    this.play();
+//}, false);
+//
+//introVideo.play();
 
 // $('.moreSessionInfo:empty').each(function() {
 //     $(this).removeClass("moreSessionInfo").closest('.singleSession').css("cursor","inherit");
